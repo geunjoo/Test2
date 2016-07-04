fcf0df740fed11dfc0198d1d17b0fd97081ecfbf
geunjoo <zlfk1234@gmail.com>
2016-07-05 04:55:17 +0900

    add blank activity and xml

---
+++ app/src/main/java/com/example/user/test/BlankActivity.java
@@ -0,0 +1,18 @@
+package com.example.user.test;
+
+
+import android.os.Bundle;
+
+/**
+ * Created by WFUser on 2016-07-05.
+ */
+public class BlankActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blank);
+    }
+
+
+
+}
